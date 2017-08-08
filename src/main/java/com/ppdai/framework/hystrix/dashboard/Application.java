7ebd5a2cb6a2c15e11ad52e83290acdac841d6fd
yinzuolong <yinzuolong@ppdai.com>
2017-08-08 11:15:59 +0800

    增加健康检查页面

--- src/main/java/com/ppdai/framework/hystrix/dashboard/Application.java
+++ src/main/java/com/ppdai/framework/hystrix/dashboard/Application.java
@@ -3,12 +3,20 @@ package com.ppdai.framework.hystrix.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableHystrixDashboard
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/hs")
+	public String hs(){
+		return "OK";
 	}
 }
